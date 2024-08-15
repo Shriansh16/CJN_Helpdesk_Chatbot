e2201d0136cf3245fc2a5080d50120c1088649f2
Shriansh16 <shrianshsingh20@gmail.com>
2024-08-15 20:28:35 +0530

    first commit

---
+++ pdf_loader.py
@@ -0,0 +1,7 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import PyPDFLoader
+
+def load_pdf(pdf_path):
+    loader=DirectoryLoader(pdf_path,glob='*.pdf',loader_cls=PyPDFLoader)
+    document=loader.load()
+    return document
